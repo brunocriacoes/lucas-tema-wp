fad1b2f55f55b7af118ddedb44e5ae98653d93b5
brunocriacoes <br.rafael@outlook.com>
2019-04-20 16:21:39 -0300

    html

---
+++ html-lucas/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>dash</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/boot.css">
+</head>
+<body>
+ <div class="container">
+   <div class="grid-2">	
+   	 <div><!-- direita -->
+   
+   <aside class="aside">
+  <header>
+  			
+	<p>HOT DEAL</p>
+	<h3>Santora <br>Foros</h3>
+   
+   </header>
+
+   	<div class="footer_aside">
+   		<p>3 Days | 310km</p>
+   		<img src="assets/image/ft.png">
+   	</div>
+  </aside>
+    </div><!-- fim direita -->
+<!-- FIM SECTION MAIN-->
+
+<!-- ASIDE-->
+    <div class="bg-fff"><!-- esquerda -->
+   <!-- SECTION MAIN-->
+   <section class="secao_ar">
+   	<!-- HEADER MAIN-->	
+  <header class="header">
+	<nav class="nav">
+	  <ul>
+		<li class="mark"><a href="" title="">Newest</a></li>
+		<li><a href="" title="">Popular</a></li>
+		<li><a href="" title="">All</a></li>
+	  </ul>
+	</nav>
+	</header><!-- FIM HEADER MAIN-->
+
+     <article>
+      	<header>
+      	 <div class="grid-a">	
+         <div>
+         	<h1>Dream between <br>Two worlds</h1>
+         </div>
+         
+         <div>
+   	  	 <p><span>$1200</span></p>
+         </div>
+   	     </div>
+   	  </header>
+         <div class="content">
+   	   	   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+   	   	   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+   	   	   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+   	   	   consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+   	   	   cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+   	   	   proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+   	   	  </div>  
+   	   	  <div class="grid-a">
+           
+           <div>
+   	      	<ul class="a">
+   	      		<li>Level: <strong>Comfort</strong></li>
+   	      		<li>Level: <strong>3 Days</strong></li>
+   	      	</ul>
+   	       </div>
+   	       <div class="lerm"> 	
+   	      	<a href="">Learn more</a>
+   	       </div>
+   	      </div>
+   	   	 </article>  
+   
+   	      
+         <div class="grid-3">
+         <div>
+         	<img src="assets/image/html.png" alt="html">	 
+         </div>
+         <div>
+         	<img src="assets/image/html.png" alt="html">	 
+         </div>
+         <div>
+         	<img src="assets/image/html.png" alt="html">	 
+         </div>	
+         	
+         </div>
+
+   	   </section>
+   
+   </section>
+    </div><!-- fim esquerda -->
+  </div>
+ </div>
+</body>
+</html>
